76f1b1fa7f1b7ffb0c199f9102e30cac79dbaa8d
Ayaz Fazlani <ayazfazlani@gmail.com>
2026-04-18 11:34:39 +0500

    robot-tag-added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
     // icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚗</text></svg>",
     icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚘</text></svg>",
   },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
